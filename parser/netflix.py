30a19151ecab63976aee6197a414024cc67a5fb9
Suchit Parikh <suchit.parikh@sendgrid.com>
2013-05-01 18:36:52 -0700

    Added key, etc. to config file

--- parser/netflix.py
+++ parser/netflix.py
@@ -1,12 +1,25 @@
 from pyflix2 import NetflixAPIV2
 from movie import movie
+import ConfigParser
+import os
 
 class Netflix(object):
     """
     Netflix API
     """
+    
+    section = 'netflix'
+    config = ConfigParser.SafeConfigParser()
+    config.readfp(open('../config.cfg'))
+    if not config.has_section(section):
+        # error condition
+        pass
+
+    KEY=config.get(section,'KEY')
+    SHARED_SECRET=config.get(section,'SHARED_SECRET')
+
     def __init__(self):
-        self.service = NetflixAPIV2('Where Is Movie', '', '')
+        self.service = NetflixAPIV2('Where Is Movie', KEY, SHARED_SECRET)
 
     def get_movie_by_title(self, title, filter=None):
         '''
@@ -16,10 +29,10 @@ class Netflix(object):
         '''
         res = self.service.get_movie_by_title(title, filter=filter)
         m_details = self.service.get_title(res.get('id'), category="format_availability")
-        
+
         print '\n{0}\n'.format(m_details)
         m = movie(str(res.get('title')), 'Netflix')
-        
+
         # Check for formats
         if m_details.get('delivery_formats'):
             f = m_details.get('delivery_formats')
@@ -27,6 +40,6 @@ class Netflix(object):
                 m.DVD = True
             if f.get('instant'):
                 m.stream = True
-        
+
         # return the movie json
         return m
--- parser/youtube.py
+++ parser/youtube.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
 
+from apiclient.discovery import build
+import ConfigParser
+import json
+import urllib
+
 class Youtube(object):
     """
     Youtube API parser
     """
-    
+    section = "youtube"
+    config = ConfigParser.SafeConfigParser()
+    config.readfp(open('../config.cfg'))
+    if not config.has_section(section):
+        # error condition
+        pass
+
+    DEVELOPER_KEY = config.get(section, 'DEVELOPER_KEY')
+    YOUTUBE_API_SERVICE_NAME = config.get(section, 'YOUTUBE_API_SERVICE_NAME')
+    YOUTUBE_API_VERSION = "V3"
+
     def __init__(self):
         API_KEY = ""
         part = "contentDetails"
